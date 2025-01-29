5e6df4e871a0a21c601828419987433b2737f676
Arpita2209 <arpitasarode1@gmail.com>
2025-01-29 23:25:30 +0530

    Add files via upload

---
+++ takinginput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class takinginput {
+    public static void main(String[] args)
+    {
+        Scanner SC=new Scanner(System.in);
+        String name=SC.next();
+        System.out.print(name);
+
+
+    }
+    
+}
