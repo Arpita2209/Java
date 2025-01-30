b72735a50708a3e353552815d4d9bc5e07d116c1
Arpita2209 <arpitasarode1@gmail.com>
2025-01-30 23:18:58 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>18)
+        {
+            System.out.println("adult");
+        }
+        else
+        {
+            System.out.println("not adult");
+        }
+
+
+    }
+    
+}
